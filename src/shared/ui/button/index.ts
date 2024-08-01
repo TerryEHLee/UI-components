19aff23993644be726000293b2f2bbb7acb0d7d7
TerryEHLee <terryehlee@gmail.com>
2024-08-01 22:15:26 +0900

    [refactor]: button
    1. outlined, contained, icon 3가지 버튼 컴포넌트를 하나로 합친 후 해당 컴포넌트에서 export
    2. 각 버튼의 type을 분리함

---
+++ src/shared/ui/button/index.ts
@@ -0,0 +1,5 @@
+export {ContainedButton} from '@/shared/ui/button/ContainedButton.tsx'
+
+export {IconButton} from '@/shared/ui/button/IconButton.tsx';
+
+export {OutlinedButton} from '@/shared/ui/button/OutlinedButton.tsx'
