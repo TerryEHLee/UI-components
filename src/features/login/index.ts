6a29c27f4cc68eef0c09677b5a06c3fe431fb84f
TerryEHLee <terryehlee@gmail.com>
2024-08-01 20:59:29 +0900

    [feature]: login page 구현
    1. feature, page 인덱스: index.ts
    2. 로그인 페이지 ui: LoginForm.tsx
    3. 로그인 페이지 옵션(id 저장, pw 찾기): LoginOptions.tsx
    4. 유효성검사: loginFormSchema.ts,

---
+++ src/features/login/index.ts
@@ -0,0 +1,4 @@
+export { loginFormSchema } from "@/features/login/model/loginFormSchema.ts"
+export type {LoginFormSchema} from "@/features/login/model/loginFormSchema.ts"
+export { LoginOptions } from '@/features/login/ui/LoginOptions.tsx';
+export {LoginForm} from '@/features/login/ui/LoginForm.tsx'
---
+++ src/pages/login/ui/Page.tsx
@@ -0,0 +1,5 @@
+import { LoginForm } from "../../../features/login/ui/LoginForm.tsx";
+
+export const LoginPage = () => {
+  return <LoginForm />;
+};
