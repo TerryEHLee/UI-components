643a93d8005e99078125a7808a109ca5dad9a982
TerryEHLee <terryehlee@gmail.com>
2024-08-06 11:09:23 +0900

    [feature]: theme 적용
    1. mui에서 제공하는 theme을 provider로 감싸 적용
    2. 기타 css를 less를 import 하여 적용함

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { appRouter } from "@/app/appRouter";
 import { RouterProvider } from "react-router-dom";
+import { appTheme } from "@/shared/ui/theme";
+import { ThemeProvider } from "@mui/material";
+import '@/shared/styles/_common.less'
+import '@/shared/styles/_fonts.less'
+import '@/shared/styles/_reset.less'
+import '@/shared/styles/_utils.less'
+import '@/shared/styles/appTheme-overrides.less'
+import '@/shared/styles/layout.less'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ThemeProvider theme = {appTheme}>
     <RouterProvider router={appRouter()} />
+    </ThemeProvider>
   </React.StrictMode>
 );
